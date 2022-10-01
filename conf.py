f0028b6884586667b691f745ee9ae0bd28a906bb
Paulo Loma Marconi <zurits@gmail.com>
2022-10-02 00:27:01 +0100

    Nikola auto commit.
    Nikola version: 8.2.3

--- conf.py
+++ conf.py
@@ -278,7 +278,7 @@ INDEX_PATH = "blog"
 # (e.g. 'Europe/Zurich')
 # Also, if you want to use a different time zone in some of your posts,
 # you can use the ISO 8601/RFC 3339 format (ex. 2012-03-30T23:00:00+02:00)
-TIMEZONE = "America/La_Paz"
+TIMEZONE = "Europe/London"
 
 # If you want to use ISO 8601 (also valid RFC 3339) throughout Nikola
 # (especially in new_post), set this to True.
--- pages/cheatsheet.rst
+++ pages/cheatsheet.rst
@@ -38,7 +38,7 @@ https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c68
 
 .. code-block:: bash
 
-    conda create -name <env_name> python=3.6
+    conda create --name <env_name> python=3.6
     conda env list
     conda env remove --name <env_name>
 
